a43b117f48cad2fc9d6915680b95c17e86702e7a
Christopher <cnhamilton1@yahoo.com>
2021-11-09 08:32:33 -0700

    modified photo.js

--- models/Photo.js
+++ models/Photo.js
@@ -10,6 +10,16 @@ const b2 = new B2({
     applicationKey: process.env.B2_KEY
 })
 
+const authorize = async function () {
+  await b2.authorize().then((result) => {
+    console.log(result.headers.date)
+    return result
+  })
+}
+
+const runAuthorize = setTimeout(function() {authorize()}, 86400000) 
+
+
 const storage = multer.memoryStorage()
 const multerUploads = multer({storage: storage}).any()
 
@@ -18,25 +28,10 @@ const Photo = function(data) {
     this.photo = data
 }
 
-Photo.prototype.photoBuckets = async function GetBucket() {
-    try {
-      await b2.authorize()
-      let response = await b2.listFileNames({bucketId: '35abac16599ccd7073c8031d'})
-      return response.data
-    } catch (err) {
-      console.log('Error getting bucket:', err);
-    }
-  }
-
-
-/*Photo.prototype.photoBuckets().then(function(result) {
-    console.log(result)
- })*/
-
 
 Photo.prototype.uploadPhoto = async function(id) {
     let filename = encodeURI(id)
-    await b2.authorize()
+    await authorize()
     return new Promise((resolve, reject) => {
     
     const uploadPromises = this.photo.map(async function(photo){
@@ -66,7 +61,7 @@ Photo.prototype.uploadPhoto = async function(id) {
 
  Photo.getPhotoUrl = function(name) {
      return new Promise(async function(resolve, reject) {
-        let authToken = await b2.authorize()
+        let authToken = await authorize()
         let url = authToken.data.downloadUrl
         let photoUrl = url+'/file/'+'heimursaga-entry-photos'+'/'+name
 
@@ -82,7 +77,7 @@ Photo.prototype.uploadPhoto = async function(id) {
 
  Photo.delete = function(photoNameToDelete) {
     return new Promise(async (resolve, reject) => {
-    await b2.authorize()
+    await authorize()
       try {
         let photo = await b2.listFileNames({
             bucketId: process.env.ENTRY_PHOTO_BUCKETID,
@@ -103,32 +98,6 @@ Photo.prototype.uploadPhoto = async function(id) {
       }
     })
   }
-
-
-Photo.checkPhoto = async function(name) {
-  await b2.authorize()
-  return new Promise((resolve, reject) => {
-
-  b2.listFileNames({
-    bucketId: process.env.ENTRY_PHOTO_BUCKETID,
-    startFileName: name,
-    maxFileCount: 1
-}).then((photo) => {
-  if (photo.data.files[0].fileName === name) {
-    resolve(true)
-  } else {
-    resolve(false)
-  }
-}).catch(() => {
-  reject(false)
-})
-})
-}
-
-/*let name = "618856b573b36e5472c395e7"
-Photo.checkPhoto(name).then((result) => {
-    console.log(result)
-})*/
 
 
 
