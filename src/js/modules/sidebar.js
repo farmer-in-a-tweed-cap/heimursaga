400f7bd661669838cfe7c9ae38a3566aab446ad4
Christopher <cnhamilton1@yahoo.com>
2023-09-14 17:25:17 -0400

    sidebar added to localmemory

--- src/js/modules/sidebar.js
+++ src/js/modules/sidebar.js
@@ -2,17 +2,30 @@
 import SimpleBar from "simplebar";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  const sidebarElement = document.getElementsByClassName("sidebar")[0];
+  const sidebarToggleElement = document.getElementsByClassName("sidebar-toggle")[0];
+
+  if (localStorage.getItem("displaySidebar") == "false") {
+    sidebarElement.classList.toggle("collapsed")
+  }
+
   const simpleBarElement = document.getElementsByClassName("js-simplebar")[0];
   
   if(simpleBarElement){
     /* Initialize simplebar */
     new SimpleBar(document.getElementsByClassName("js-simplebar")[0])
   
-    const sidebarElement = document.getElementsByClassName("sidebar")[0];
-    const sidebarToggleElement = document.getElementsByClassName("sidebar-toggle")[0];
   
     sidebarToggleElement.addEventListener("click", () => {
-      sidebarElement.classList.toggle("collapsed");
+      if (localStorage.getItem("displaySidebar") == "false") {
+        localStorage.setItem("displaySidebar", "true")
+        sidebarElement.classList.toggle("collapsed")
+      } else {
+        localStorage.setItem("displaySidebar", "false")
+        sidebarElement.classList.toggle("collapsed")
+      }
+
   
       sidebarElement.addEventListener("transitionend", () => {
         window.dispatchEvent(new Event("resize"));
