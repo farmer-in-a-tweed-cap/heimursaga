ed870ca96276da4e83db8f792be5367dd8448406
Christopher <cnhamilton1@yahoo.com>
2023-12-14 14:11:24 -0500

    mods

--- src/js/modules/sponsor.js
+++ src/js/modules/sponsor.js
@@ -390,7 +390,7 @@ export default class Sponsor {
             }
           )
             .then(async (result) => {
-              console.log(result.json())
+              console.log(result)
               const response = await result.json();
               fundSubmitBtn.classList.remove("disabled");
               fundSubmitBtn.innerHTML = "Submit";
@@ -401,15 +401,11 @@ export default class Sponsor {
                 parent.window.notyf.success(`Sponsor payment to ${this.username} successful!`);
               } else {
                 parent.window.notyf.error({background: '#ac4946', message: `Payment unsuccessful`});
-                const error = document.getElementById("error");
-                error.innerHTML = "Payment unsuccessful";
               }
               return response;
             })
             .catch((err) => {
               parent.window.notyf.error({background: '#ac4946', message: `Payment unsuccessful`});
-              const error = document.getElementById("error");
-              error.innerHTML = "Payment unsuccessful";
               fundSubmitBtn.classList.remove("disabled");
               fundSubmitBtn.innerHTML = "Submit";
             });
